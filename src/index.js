d85fce0c8e5100d587204f7b3e7d5c7d48d20c73
Student <example@example.com>
2020-02-06 09:55:16 -0800

    Add functionality for generating an item with random strength, intelligence, required strength, required intelligence, and name.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import $ from 'jquery';
-import { Character } from './js/character';
-import { Type } from './js/type';
+// import { Character } from './js/character';
+// import { Type } from './js/type';
+// import { isTerminatorless } from '@babel/types';
+import { Item } from './js/item';
 
 
 
 $(document).ready(function() {
-    let type = new Type('warrior');
-    let character = new Character(2,2,2, type);
-    character.setCurrentExperience(100);
+    let item = new Item(1);
 
-   // console.log(character.skills[0]() + " char skills");
+    console.log(item.getName());
+    let itemModifierArray = item.getListOfItemModifiers();
+    for(let i = 0; i < itemModifierArray.length; i++) {
+        
+        console.log(itemModifierArray[i] + " item modifier");
+    }
+    console.log(item.getStrength());
+    console.log(item.getIntelligence());
+    console.log(item.getStrengthReq() + " strength req");
+    console.log(item.getIntelligenceReq() + " int req");
 });
--- src/js/item.js
+++ src/js/item.js
@@ -1,23 +1,85 @@
 export class Item {
-    constructor(strengthReq, intelligenceReq) {
-        this.strengthReq = strengthReq;
-        this.intelligenceReq = intelligenceReq;
-        this.strength = strengthReq * 2;
-        this.intelligence = intelligenceReq * 2;
-    }
+    constructor(playerLevel) {
+      this.strength = playerLevel * 2;
+      this.intelligence = playerLevel * 2;
 
+      this.name = this.createRandomItemName();
+      this.listOfUniqueItemModifiers = this.createRandomMultipliers();
+      this.strengthReq = (this.strength / 2);
+      this.intelligenceReq = (this.intelligence / 2);
+    }
+  getName() {
+      return this.name;
+  }
     getStrength() {
-        return this.strength;
+      return this.strength;
     }
     getIntelligence() {
-        return this.intelligence;
+      return this.intelligence;
     }
-
     getStrengthReq() {
-        return this.strengthReq;
+      return this.strengthReq;
     }
     getIntelligenceReq() {
-        return this.intelligenceReq;
+      return this.intelligenceReq;
+    }
+    getSellValueInGold() {
+      //sell value in gold should be dependent on the str and damage
     }
 
-}
+    getListOfItemModifiers() {
+      return this.listOfUniqueItemModifiers;
+    }
+    createRandomItemName() {
+      let firstNamesArray = ["The","Daring","Mystic","Searing","Witches"];
+      let secondNamesArray = ["Killer","Boom","King"];
+      let namesToChooseFrom = [firstNamesArray, secondNamesArray];
+      let numberOfNamesToAdd = namesToChooseFrom.length;
+      let generatedItemName = "";
+      
+      for(let i = 0; i < numberOfNamesToAdd; i++) {
+        let nameArrayToSelectNameFrom = namesToChooseFrom[i];
+        let value = Math.floor((Math.random() * (nameArrayToSelectNameFrom.length)));
+          if(i === (numberOfNamesToAdd-1)) {
+            generatedItemName += nameArrayToSelectNameFrom[value];
+          } else {
+            generatedItemName += nameArrayToSelectNameFrom[value] + " ";
+          }
+      }
+      return generatedItemName;
+    }
+      
+    createRandomMultipliers() {
+      let numberOfAddedItemModifiers = Math.floor((Math.random() * 4));
+      let listOfUniqueItemModifiers = [];
+      //console.log(numberOfAddedItemModifiers + " numberofaddeditemmodifiers");
+  
+      let operators = ['*','+'];
+      //console.log(operators.length + " operators length");
+      if(numberOfAddedItemModifiers != 0) {
+        for(let i = 0; i < numberOfAddedItemModifiers; i++) {
+          let randomlyGeneratedValueToGetAttribute = Math.floor((Math.random() * 2));
+          //console.log(randomlyGeneratedValueToGetAttribute + " # to select attribute");
+          let attributeToModify = Object.keys(this)[randomlyGeneratedValueToGetAttribute];
+          //console.log(attributeToModify + " attribute to modify");
+          let randomlySelectedOperatorNumber = Math.floor((Math.random() * operators.length));
+          //console.log(randomlySelectedOperatorNumber + " randomly selected operator number")
+          let randomlySelectedOperator = operators[randomlySelectedOperatorNumber];
+          //console.log(randomlySelectedOperator + " randomly selected operator")
+          let randomlyGeneratedModifierNumber = Math.floor((Math.random() * 2)) + 1;
+          if (randomlySelectedOperator === '+') {
+            this[`${attributeToModify}`] = this[`${attributeToModify}`] + randomlyGeneratedModifierNumber;
+            listOfUniqueItemModifiers.push(`+ ${randomlyGeneratedModifierNumber} to ${Object.keys(this)[randomlyGeneratedValueToGetAttribute]}`);
+            //console.log(Object.keys(this)[randomlyGeneratedValueToGetAttribute] + " object.keys");
+            //console.log(attributeToModify + " in +");
+          } else {
+            this[`${attributeToModify}`] = this[`${attributeToModify}`] * randomlyGeneratedModifierNumber;
+            listOfUniqueItemModifiers.push(`${randomlyGeneratedModifierNumber} to ${Object.keys(this)[randomlyGeneratedValueToGetAttribute]}`);
+            //console.log(attributeToModify + " in *");
+            //console.log(Object.keys(this)[randomlyGeneratedValueToGetAttribute] + " object.keys");
+          }
+        }
+      }
+      return listOfUniqueItemModifiers;
+    }
+  }
